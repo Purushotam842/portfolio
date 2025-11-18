3c681cbfcdc7e381bdf09b8c0496c79fd9d0d42f
purushotam842 <purushotamkumar842@gmail.com>
2025-11-19 00:12:59 +0530

    This is for upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+const texts = ["Frontend Developer.", "MERN Stack Developer.", "IOT Developer."];
+let textIndex = 0;
+let charIndex = 0;
+const typingElement = document.getElementById("typing");
+
+function typeEffect() {
+  if (charIndex < texts[textIndex].length) {
+    typingElement.textContent += texts[textIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(typeEffect, 100);
+  } else {
+    // Wait before deleting
+    setTimeout(eraseEffect, 1500);
+  }
+}
+
+function eraseEffect() {
+  if (charIndex > 0) {
+    typingElement.textContent = texts[textIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(eraseEffect, 50);
+  } else {
+    // Move to next text
+    textIndex = (textIndex + 1) % texts.length;
+    setTimeout(typeEffect, 500);
+  }
+}
+
+window.onload = typeEffect;
+
+window.addEventListener("load", () => {
+    const fills = document.querySelectorAll(".skill-bar-fill");
+    fills.forEach(fill => {
+      const percent = fill.getAttribute("data-percent");
+      fill.style.width = percent + "%";
+    });
+  });
+  
